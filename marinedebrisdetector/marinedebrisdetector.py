0a01a9ff650cc547b2d2e8671d609cbadb72419e
Marc Rußwurm <marc.russwurm@tum.de>
2022-12-09 19:58:59 +0100

    added hubconf

---
+++ marinedebrisdetector/marinedebrisdetector.py
@@ -0,0 +1,16 @@
+import torch
+from torch import nn
+from checkpoints import CHECKPOINTS
+class MarineDebrisDetector(nn.Module):
+
+    def __init__(self):
+        self.model = ""
+        pass
+
+    def forward(self):
+        pass
+def main():
+    detector = MarineDebrisDetector()
+
+if __name__ == '__main__':
+    main()
