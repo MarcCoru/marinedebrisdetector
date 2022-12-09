a8b8568bab62742dd3894a1b2baa3f49bfa25104
Marc Rußwurm <marc.russwurm@tum.de>
2022-12-09 19:34:39 +0100

    added hubconf

---
+++ hubconf.py
@@ -0,0 +1,12 @@
+dependencies = ["torch", "segmentation-models-pytorch", "pytorch_lightning"]
+
+from marinedebrisdetector.model.segmentation_model import SegmentationModel
+from marinedebrisdetector.checkpoints import CHECKPOINTS
+
+def unetpp(seed):
+    assert seed in [1,2,3]
+    return SegmentationModel.load_from_checkpoint(CHECKPOINTS[f"unet++{seed}"])
+
+def unet(seed):
+    assert seed in [1, 2, 3]
+    return SegmentationModel.load_from_checkpoint(CHECKPOINTS[f"unet{seed}"])
