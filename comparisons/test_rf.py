e6490b21efa8a2c0414149fd2133e598812a56cc
Lazy-Kyu <sushen98@hotmail.com>
2022-12-12 18:53:03 +0100

    test_rf refactored

--- comparisons/test_rf.py
+++ comparisons/test_rf.py
@@ -1,38 +1,19 @@
 import sys
 sys.path.append("..")
-sys.path.append("/home/sushen/marine_debris_semester_project")
+import os
+from os.path import dirname as up
+sys.path.append(os.path.join(up(up(os.path.abspath(__file__))), 'marinedebrisdetector', 'data'))
+
 import joblib
 import numpy as np
-from tqdm import tqdm
 from sklearn.metrics import classification_report
-
-from data.refined_floatingobjects import RefinedFlobsRegionDataset
-from data.marida import MaridaDataset, CLASS_MAPPING_USED
-CLASS_MAPPING_USED_INV = {v:k for k,v in  CLASS_MAPPING_USED.items()}
-from metrics import calculate_metrics
+import argparse 
+from marida import CLASS_MAPPING_USED
+CLASS_MAPPING_USED_INV = {v:k for k,v in CLASS_MAPPING_USED.items()}
 
 L2ABANDS = ["B1", "B2", "B3", "B4", "B5", "B6", "B7", "B8", "B8A", "B9", "B11", "B12"]
 marida_bands = ["B1", "B2", "B3", "B4", "B5", "B6", "B7", "B8", "B8A", "B11", "B12"]
 marida_band_idxs = np.array([L2ABANDS.index(b) for b in marida_bands])
-
-def transform(image):
-    img = image[marida_band_idxs]
-    C, H, W = img.shape
-
-    # center pixels
-    x = img[:, H // 2, W // 2]
-    return x
-
-def transform_image(image, msk):
-    img = image[marida_band_idxs] * 1e-4
-
-    C, H, W = img.shape
-
-    # center pixels
-    x = img[:, H // 2, W // 2]
-    y = msk[H // 2, W // 2]
-
-    return x, y
 
 from sklearn.metrics import precision_recall_fscore_support, cohen_kappa_score, jaccard_score, accuracy_score, roc_auc_score
 def calculate_metrics(targets, predictions, scores):
@@ -56,21 +37,15 @@ def calculate_metrics(targets, predictions, scores):
         jaccard=jaccard
     )
 
-def predict_refined_floatingobjects(model, region):
+def predict_refined_floatingobjects(model, region, test_data_path):
     # Load test dataset
-    f = np.load('/data/sushen/marinedebris/results/kikaki/randomforest/randomforest/test.npz')
+    f = np.load(test_data_path)
     X = f["X"]
     y = f["y"]
     id = f["id"]
 
     # Difference between l1c and l2a is that we remove Band 10. So now need to remove Band 9 to use Marida RF.
     X = np.delete(X, 9, axis=1)
-
-    # Must keep features based on type of random forest trained on Marida
-    if rf_number == 0:
-        X = X[:, :11]
-    if rf_number == 1:
-        X = X[:, :19]
 
     datapoint_region = np.array([i.split("-")[0] for i in id])
     if region == "accra_20181031":
@@ -94,21 +69,15 @@ def predict_refined_floatingobjects(model, region):
     print((y_binary == y).mean())
     print(classification_report(y_true=y, y_pred=y_binary))
 
-def predict_marida(model):
+def predict_marida(model, test_data_path):
     # Load test dataset
-    f = np.load('/data/sushen/marinedebris/results/kikaki/randomforest/randomforest/test.npz')
+    f = np.load(test_data_path)
     X = f["X"]
     y = f["y"]
     id = f["id"]
 
     # Difference between l1c and l2a is that we remove Band 10. So now need to remove Band 9 to use Marida RF.
     X = np.delete(X, 9, axis=1)
-
-    # Must keep features based on type of random forest trained on Marida
-    if rf_number == 0:
-        X = X[:, :11]
-    if rf_number == 1:
-        X = X[:, :19]
 
     datapoint_region = np.array([i.split("-")[0] for i in id])
     indexes = datapoint_region == "marida"
@@ -133,19 +102,28 @@ def predict_marida(model):
 
     print((y_binary == y).mean())
 
-# 0, 1 or 2 to choose which rf model to be used
-rf_number = 2
-rf_classifier_paths = ['/data/sushen/marinedebris/MARIDA/rf_classifier_ss.joblib', 
-    '/data/sushen/marinedebris/MARIDA/rf_classifier_ss_si.joblib', 
-    '/data/sushen/marinedebris/MARIDA/rf_classifier_ss_si_glcm.joblib']
-rf_classifier_path = rf_classifier_paths[rf_number]
-model = joblib.load(rf_classifier_path)
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--ckpt-path', type=str, default="/data/marinedebris/marida_classifier/rf_classifier_marida_original.joblib")
+    parser.add_argument('--data-path', type=str, default="/data/marinedebris/results/kikaki/randomforest/test.npz")
 
-print("durban")
-predict_refined_floatingobjects(model, "durban_20190424")
+    args = parser.parse_args()
 
-print("accra")
-predict_refined_floatingobjects(model, "accra_20181031")
+    return args
 
-print("marida")
-predict_marida(model)
+def main(args):
+    rf_classifier_path = args.ckpt_path
+    test_data_path = args.data_path
+    model = joblib.load(rf_classifier_path)
+
+    print("durban")
+    predict_refined_floatingobjects(model, "durban_20190424", test_data_path)
+
+    print("accra")
+    predict_refined_floatingobjects(model, "accra_20181031", test_data_path)
+
+    print("marida")
+    predict_marida(model)
+    
+if __name__ == '__main__':
+    main(parse_args())
